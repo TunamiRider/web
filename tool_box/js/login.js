8c09454b96b22d67e73b818b2cf3c8043dc7a348
TunamiRider <yoite.sy@gmail.com>
2020-08-13 17:55:19 -0500

    Create login.js

---
+++ tool_box/js/login.js
@@ -0,0 +1,10 @@
+function validate(){
+  var username = document.getElementById("uname").value;
+  var password = document.getElementById("psw").value;
+  
+  if( username == "yuki" && password == "123" ){
+    alert("Login successfully");
+    return false;
+  }
+  
+}
