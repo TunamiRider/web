53952201a9b96ec6c9ddc2a09150ad5163e6da9e
TunamiRider <yoite.sy@gmail.com>
2020-08-14 19:08:07 -0500

    Update script.js

--- tool_box/js/script.js
+++ tool_box/js/script.js
@@ -1,15 +1,15 @@
 function validate(){
-  //var username = document.getElementById("uname").value;
-  //var password = document.getElementById("psw").value;
+  var username = document.getElementById("uname").value;
+  var password = document.getElementById("psw").value;
   
-  console.log("TTTTTT"); 
+  console.log(username +" "+password); 
   
-  /*if( username == "yuki" && password == "123" ){
+  if( username == "yuki" && password == "123" ){
     alert("Login successfully");
     
   }
   else{
     alert("Login failed");
-  }*/
+  }
   return false;
 }
