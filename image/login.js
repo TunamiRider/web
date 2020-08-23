7a2666c458f6ab57651f2dbfc277c24b4a8919a9
TunamiRider <yoite.sy@gmail.com>
2020-08-23 11:11:04 -0500

    Add files via upload

---
+++ image/login.js
@@ -0,0 +1,15 @@
+function validate(){
+  //var username = document.getElementById("uname").value;
+  //var password = document.getElementById("psw").value;
+  
+  console.log("TTTTTT"); 
+  
+  /*if( username == "yuki" && password == "123" ){
+    alert("Login successfully");
+    
+  }
+  else{
+    alert("Login failed");
+  }*/
+  return false;
+}
