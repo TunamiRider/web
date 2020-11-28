433965ba0cca5471a1473daafefc92f72eb96c16
TunamiRider <yoite.sy@gmail.com>
2020-11-27 19:38:55 -0600

    Update script.js

--- tool_box/js/script.js
+++ tool_box/js/script.js
@@ -6,8 +6,8 @@ function validate(){
   
   if( username == "yuki" && password == "123" ||
       username == "satoshi" && password == "456" ){
-    //window.location.assign("../../web/pages/twolinks.html");
-    document.location = '../../web/pages/twolinks.html';
+    window.location.assign("http://example.com");
+    //document.location = '../../web/pages/twolinks.html';
     alert("Login successfully");
     
   }
