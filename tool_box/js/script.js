f74ca83093c62fab88da66b60ccd8079ebcf48b8
TunamiRider <yoite.sy@gmail.com>
2022-07-20 15:59:54 -0500

    Update script.js

--- tool_box/js/script.js
+++ tool_box/js/script.js
@@ -8,7 +8,8 @@ function validate(){
       (username == "satoshi" && password == "456") ){
     //window.location.assign("./pages/twolinks.html");
     //document.location = './hello.html';
-    window.location.replace("../../hello.html");
+    //window.location.replace("../../hello.html");
+    window.location.replace("https://tunamirider.github.io./web/hello.html");
     alert("Login successfully");
     
   }
